f57c6490247c6d043873d025b71aebca941b35de
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-11-01 12:50:39 +0900

    fix the spec

--- tests/serverspec/fixed_ip_spec.rb
+++ tests/serverspec/fixed_ip_spec.rb
@@ -6,6 +6,8 @@ describe file('/etc/resolv.conf') do
     its(:content) { should match /nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.2/ }
   when 'fixed-ip-freebsd-103-amd64'
     its(:content) { should match /nameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3/ }
+  when 'fixed-ip-centos-72-x86-64'
+    its(:content) { should match /nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1/ }
   else
     its(:content) { should match /nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/ }
   end
