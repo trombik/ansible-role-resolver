5e7514d755ce4fb59ebf02e7f933ba639d85f42f
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-11-01 14:52:30 +0900

    support ubuntu
    fixes #4

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -10,14 +10,21 @@ when 'centos'
     its(:content) { should match /PEERDNS=no/ }
     its(:content) { should_not match /PEERDNS=yes/ }
   end
+when 'ubuntu'
+  describe file('/etc/dhcp/dhclient.conf') do
+    its(:content) { should match /^supersede domain-name-servers 192\.168\.1\.3,192\.168\.1\.2,192\.168\.1\.1;/ }
+  end
 end
 
 describe file('/etc/resolv.conf') do
+  its(:content) { should_not match /nameserver\s+#{ Regexp.escape('10.0.2.3') }/ }
   case host_inventory['fqdn']
   when 'default-freebsd-103-amd64'
     its(:content) { should match /nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/ }
   when 'default-openbsd-60-amd64'
     its(:content) { should match /nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.3/ }
+  when 'default-ubuntu-1404-amd64'
+    its(:content) { should match /nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1/ }
   else
     its(:content) { should match /nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/ }
   end
--- tests/serverspec/fixed_ip_spec.rb
+++ tests/serverspec/fixed_ip_spec.rb
@@ -8,6 +8,8 @@ describe file('/etc/resolv.conf') do
     its(:content) { should match /nameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3/ }
   when 'fixed-ip-centos-72-x86-64'
     its(:content) { should match /nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1/ }
+  when 'fixed-ip-ubuntu-1404-amd64'
+    its(:content) { should match /nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.3/ }
   else
     its(:content) { should match /nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/ }
   end
