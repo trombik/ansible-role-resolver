07ffde0a5e88cde89fe891a601bd03fb99b1c676
Tomoyuki Sakurai <240118+trombik@users.noreply.github.com>
2018-02-15 11:37:09 +0700

    [bugfix] use ansible_hostname instead of ansible_fqdn (#1)
    also:
    
    * support OpenBSD 6.1, 6.2, FreeBSD 11.1, Ubuntu 16.04
    * QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,7 +3,12 @@ require "spec_helper"
 case os[:family]
 when "freebsd"
   describe file("/etc/resolvconf.conf") do
-    its(:content) { should match(/name_servers="192\.168\.1\.2 192\.168\.1\.3 192\.168\.1\.1"/) }
+    case host_inventory["fqdn"]
+    when "default-freebsd-103-amd64"
+      its(:content) { should match(/name_servers="192\.168\.1\.2 192\.168\.1\.3 192\.168\.1\.1"/) }
+    when "default-freebsd-111-amd64"
+      its(:content) { should match(/name_servers="192\.168\.1\.1 192\.168\.1\.3 192\.168\.1\.2"/) }
+    end
   end
 end
 
@@ -14,10 +19,14 @@ describe file("/etc/resolv.conf") do
     its(:content) { should match(/nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/) }
   when "default-openbsd-60-amd64"
     its(:content) { should match(/nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.3/) }
-  when "default-ubuntu-1404-amd64"
+  when "default-ubuntu-1404-amd64", "default-openbsd-62-amd64", "default-openbsd-61-amd64"
     its(:content) { should match(/nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1/) }
   when "default-centos-73-x86-64"
     its(:content) { should match(/nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.2/) }
+  when "default-freebsd-111-amd64"
+    its(:content) { should match(/nameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.2/) }
+  when "default-ubuntu-1604-amd64"
+    its(:content) { should match(/nameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3/) }
   else
     its(:content) { should match(/nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/) }
   end
