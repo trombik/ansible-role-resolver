f495943609f58d51e762d1c2cb5763ce2f44e378
Tomoyuki Sakurai <y@trombik.org>
2018-10-21 12:30:06 +0900

    bugfix: replace predictable_shuffle with official filter shuffle

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,35 +3,15 @@ require "spec_helper"
 case os[:family]
 when "freebsd"
   describe file("/etc/resolvconf.conf") do
-    case host_inventory["fqdn"]
-    when "default-freebsd-103-amd64"
-      its(:content) { should match(/name_servers="192\.168\.1\.2 192\.168\.1\.3 192\.168\.1\.1"/) }
-    when "default-freebsd-111-amd64"
-      its(:content) { should match(/name_servers="192\.168\.1\.1 192\.168\.1\.3 192\.168\.1\.2"/) }
-    end
+    its(:content) { should match(/# Managed by ansible$/) }
+    its(:content) { should match(/^name_servers=".*"$/) }
   end
 end
-puts host_inventory["fqdn"]
 describe file("/etc/resolv.conf") do
   its(:content) { should match(/# Managed by ansible$/) }
-  its(:content) { should_not match(/nameserver\s+#{ Regexp.escape('10.0.2.3') }/) }
-  case host_inventory["fqdn"]
-  when "default-freebsd-103-amd64"
-    its(:content) { should match(/nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/) }
-  when "default-openbsd-60-amd64"
-    its(:content) { should match(/nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.3/) }
-  when "default-ubuntu-1404-amd64", "default-openbsd-62-amd64", "default-openbsd-61-amd64"
-    its(:content) { should match(/nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.1/) }
-  when "default-centos-73-x86-64"
-    its(:content) { should match(/nameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.2/) }
-  when "default-freebsd-111-amd64", "default-ubuntu-1804-amd64"
-    its(:content) { should match(/nameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.2/) }
-  when "default-ubuntu-1604-amd64", "default-openbsd-63-amd64", "default-freebsd-104-amd64"
-    its(:content) { should match(/nameserver 192\.168\.1\.1\nnameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3/) }
-  else
-    its(:content) { should match(/nameserver 192\.168\.1\.2\nnameserver 192\.168\.1\.3\nnameserver 192\.168\.1\.1/) }
+  [1, 2, 3].each do |i|
+    its(:content) { should match(/^nameserver 192\.168\.1\.#{i}$/) }
   end
-
   case os[:family]
   when "openbsd"
     its(:content) { should match(/^domain i\.trombik\.org$/) }
@@ -40,4 +20,3 @@ describe file("/etc/resolv.conf") do
     its(:content) { should match(/^# beginning of resolver_config\n\n# end of resolver_config$/) }
   end
 end
-# 2 1 3
